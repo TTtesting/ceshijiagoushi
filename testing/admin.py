0c391df0c468490a8e1fe9d85c0564ee62efde50
TT <ceshijiagoushi@gmail.com>
2017-06-25 10:30:17 +0800

    优化后台功能

--- testing/admin.py
+++ testing/admin.py
@@ -2,5 +2,24 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from testing.models import Event, Guest
 
 # Register your models here.
+import sys;
+
+reload(sys);
+sys.setdefaultencoding("utf8")
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'status', 'address', 'start_time']
+    search_fields = ['name']  #搜索栏
+    list_filter = ['status']  #过滤器
+
+class GuestAdmin(admin.ModelAdmin):
+    list_display = ['realname', 'phone', 'email', 'sign', 'create_time', 'event']
+    search_fields = ['realname','phone']  #搜索栏
+    list_filter = ['sign']  #过滤器
+
+admin.site.register(Event,EventAdmin)
+admin.site.register(Guest,GuestAdmin)
+
--- testing/models.py
+++ testing/models.py
@@ -7,10 +7,10 @@ from django.db import models
 
 #发布会表
 class Event(models.Model):
-    name = models.CharField(maxlength=100) #发布会标题
+    name = models.CharField(max_length=100) #发布会标题
     limit = models.IntegerField() #参加人数
     status = models.BooleanField() #状态
-    address = models.CharField(maxlength=200) #地址
+    address = models.CharField(max_length=200) #地址
     start_time = models.DateTimeField('events time') #发布会时间
     create_time = models.DateTimeField(auto_now=True) #创建时间（自动获取当前时间）
 
@@ -19,9 +19,9 @@ class Event(models.Model):
 
 #嘉宾表
 class Guest(models.Model):
-    event = models.Foreignkey(Event) #关联发布会id
-    realname = models.CharField(maxlength=64) #姓名
-    phone = models.CharField(maxlength=16) #手机号
+    event = models.ForeignKey(Event) #关联发布会id
+    realname = models.CharField(max_length=64) #姓名
+    phone = models.CharField(max_length=16) #手机号
     email = models.EmailField() #邮箱
     sign = models.BooleanField() #签到状态
     create_time = models.DateTimeField(auto_now=True) #创建时间（自动获取当前时间）
